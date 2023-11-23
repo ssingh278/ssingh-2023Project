b6efd5936a4588ac6b04d5cd1e0c6dcd6a44387f
ssingh278 <101226227+ssingh278@users.noreply.github.com>
2023-11-23 12:29:41 -0700

    Molar mass calculator in C# and .NET
    this application you can calculate the molar mass of the any chemical formula and it will tell you that the formula is correct or not .
    
    I also used .NET Web API to create a API that has info of all element till 109, this API is also hosted on the Azure

---
+++ Molar mass Calculator using HTTP Client lib/MMC_Assignment/MMC_Assignment/MMC.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace MMC_Assignment
+{
+    partial class MMC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_DataGridView = new System.Windows.Forms.DataGridView();
+            this.UI_SortByName_Btn = new System.Windows.Forms.Button();
+            this.UI_SingleCharacterSymbols_Btn = new System.Windows.Forms.Button();
+            this.UI_SortByAtomic_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UI_ChemicalFormula_tbx = new System.Windows.Forms.TextBox();
+            this.UI_MolarMass_Tbx = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UI_DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UI_DataGridView
+            // 
+            this.UI_DataGridView.AllowUserToResizeColumns = false;
+            this.UI_DataGridView.AllowUserToResizeRows = false;
+            this.UI_DataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.UI_DataGridView.Location = new System.Drawing.Point(12, 12);
+            this.UI_DataGridView.Name = "UI_DataGridView";
+            this.UI_DataGridView.Size = new System.Drawing.Size(613, 366);
+            this.UI_DataGridView.TabIndex = 0;
+            // 
+            // UI_SortByName_Btn
+            // 
+            this.UI_SortByName_Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_SortByName_Btn.Location = new System.Drawing.Point(647, 12);
+            this.UI_SortByName_Btn.Name = "UI_SortByName_Btn";
+            this.UI_SortByName_Btn.Size = new System.Drawing.Size(141, 28);
+            this.UI_SortByName_Btn.TabIndex = 1;
+            this.UI_SortByName_Btn.Text = "Sort By Name";
+            this.UI_SortByName_Btn.UseVisualStyleBackColor = true;
+            this.UI_SortByName_Btn.Click += new System.EventHandler(this.UI_SortByName_Btn_Click);
+            // 
+            // UI_SingleCharacterSymbols_Btn
+            // 
+            this.UI_SingleCharacterSymbols_Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_SingleCharacterSymbols_Btn.Location = new System.Drawing.Point(647, 58);
+            this.UI_SingleCharacterSymbols_Btn.Name = "UI_SingleCharacterSymbols_Btn";
+            this.UI_SingleCharacterSymbols_Btn.Size = new System.Drawing.Size(141, 28);
+            this.UI_SingleCharacterSymbols_Btn.TabIndex = 2;
+            this.UI_SingleCharacterSymbols_Btn.Text = "Single Character Symbols";
+            this.UI_SingleCharacterSymbols_Btn.UseVisualStyleBackColor = true;
+            this.UI_SingleCharacterSymbols_Btn.Click += new System.EventHandler(this.UI_SingleCharacterSymbols_Btn_Click);
+            // 
+            // UI_SortByAtomic_btn
+            // 
+            this.UI_SortByAtomic_btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_SortByAtomic_btn.Location = new System.Drawing.Point(647, 104);
+            this.UI_SortByAtomic_btn.Name = "UI_SortByAtomic_btn";
+            this.UI_SortByAtomic_btn.Size = new System.Drawing.Size(141, 28);
+            this.UI_SortByAtomic_btn.TabIndex = 3;
+            this.UI_SortByAtomic_btn.Text = "Sort By Atomic #";
+            this.UI_SortByAtomic_btn.UseVisualStyleBackColor = true;
+            this.UI_SortByAtomic_btn.Click += new System.EventHandler(this.UI_SortByAtomic_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Chemical Formula : ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(519, 410);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Approx. Molar Mass :";
+            // 
+            // UI_ChemicalFormula_tbx
+            // 
+            this.UI_ChemicalFormula_tbx.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.UI_ChemicalFormula_tbx.Location = new System.Drawing.Point(117, 407);
+            this.UI_ChemicalFormula_tbx.Name = "UI_ChemicalFormula_tbx";
+            this.UI_ChemicalFormula_tbx.Size = new System.Drawing.Size(339, 20);
+            this.UI_ChemicalFormula_tbx.TabIndex = 6;
+            this.UI_ChemicalFormula_tbx.TextChanged += new System.EventHandler(this.UI_ChemicalFormula_tbx_TextChanged);
+            // 
+            // UI_MolarMass_Tbx
+            // 
+            this.UI_MolarMass_Tbx.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.UI_MolarMass_Tbx.Location = new System.Drawing.Point(647, 407);
+            this.UI_MolarMass_Tbx.Name = "UI_MolarMass_Tbx";
+            this.UI_MolarMass_Tbx.ReadOnly = true;
+            this.UI_MolarMass_Tbx.Size = new System.Drawing.Size(141, 20);
+            this.UI_MolarMass_Tbx.TabIndex = 7;
+            // 
+            // MMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UI_MolarMass_Tbx);
+            this.Controls.Add(this.UI_ChemicalFormula_tbx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UI_SortByAtomic_btn);
+            this.Controls.Add(this.UI_SingleCharacterSymbols_Btn);
+            this.Controls.Add(this.UI_SortByName_Btn);
+            this.Controls.Add(this.UI_DataGridView);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "MMC";
+            this.Text = "MMC";
+            this.Load += new System.EventHandler(this.MMC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.UI_DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView UI_DataGridView;
+        private System.Windows.Forms.Button UI_SortByName_Btn;
+        private System.Windows.Forms.Button UI_SingleCharacterSymbols_Btn;
+        private System.Windows.Forms.Button UI_SortByAtomic_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UI_ChemicalFormula_tbx;
+        private System.Windows.Forms.TextBox UI_MolarMass_Tbx;
+    }
+}
+
---
+++ Molar mass Calculator using HTTP Client lib/MMC_Assignment/MMC_Assignment/MMC.cs
@@ -0,0 +1,300 @@
+﻿//***********************************************************************************
+//Program: MMC_Assignment
+//Description:  Molecular Weight Calculator (LINQ Assignment)
+//Date: 19-Oct-2022
+//Group: Sahil Sharma,Sharry Singh
+//Course: CMPE2800
+//Class: CNT.A01(fall 2023)
+//***********************************************************************************
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Reflection;
+using System.IO;
+using MMC_Assignment.Properties;
+
+namespace MMC_Assignment
+{
+    public partial class MMC : Form
+    {
+        /// <summary>
+        /// Class containing the definition of the Atom
+        /// </summary>
+        public class Atom
+        {
+            public int AtomicNumber { get; set; } //Atomic number 
+            public string Name { get; set; }      // name of elements
+            public string Symbol { get; set; }    // Symbol of elements
+            public float MolarMass { get; set; }  // Molars of Elements
+        }
+        List<Atom> _atomList = new List<Atom>();// list to store info of all the atoms
+        BindingSource _bindingSource = new BindingSource();//binding source will be use to bind the data to Grid view
+        public MMC()
+        {
+            InitializeComponent();
+        }
+
+        private void UI_ChemicalFormula_tbx_TextChanged(object sender, EventArgs e)
+        {
+
+            Dictionary<string, int> elementDictionary = new Dictionary<string, int>();
+
+            string userInput = UI_ChemicalFormula_tbx.Text;
+
+            string pattern = @"([A-Z][a-z])(-?\d*)|([A-Z])(-?\d*)";
+
+            MatchCollection matches = Regex.Matches(userInput, pattern);
+            foreach (Match match in matches)
+            {
+                string count = null;
+                string element = null;
+                if (match.Groups[1].Success)
+                {
+                    element = match.Groups[1].Value;
+                    count = match.Groups[2].Value;
+                }
+                else if (match.Groups[3].Success)
+                {
+                    element = match.Groups[3].Value;
+                    count = match.Groups[4].Value;
+                }
+
+                if (count != null)
+                {
+                    int int_count = int.TryParse(count, out int_count) ? int_count : 1;
+                    if(int_count>= 999999999)
+                    {
+                        MessageBox.Show("999999999 is the max number allowed, it will not measure after that");
+                        UI_ChemicalFormula_tbx.Clear();
+                        _bindingSource.DataSource = null;
+                        UI_DataGridView.DataSource = _bindingSource;
+                        UI_MolarMass_Tbx.BackColor = Color.White;
+                        UI_MolarMass_Tbx.Text = "";
+                        break;
+                    }
+                    if (int_count > 0)
+                    {
+                        if (!elementDictionary.ContainsKey(element))
+                        {
+                            if (element.Length > 1)
+                                elementDictionary[$"{element[0]}"] = 1;
+                            //this happens always
+                            elementDictionary[element] = int_count;
+                        }
+                        else
+                        {
+                            elementDictionary[element] += int_count;
+                        }
+                    }
+                }
+
+            }
+
+            var elements = from kvp in elementDictionary
+                           join s in _atomList on kvp.Key equals s.Symbol
+                           select new
+                           {
+                               AtomicNumber = s.AtomicNumber,
+                               Count = kvp.Value,
+                               Name = s.Name,
+                               Symbol = s.Symbol,
+                               MolarMass = s.MolarMass,
+                               MassInFormula = s.MolarMass * kvp.Value
+                           };
+
+            if (elements.Count() > 0)
+            {
+                _bindingSource.DataSource = from item in elements
+                                            select new
+                                            {
+                                                Element = item.Name,
+                                                Count = item.Count,
+                                                Mass = item.MolarMass,
+                                                TotalMass = item.MassInFormula
+                                            };
+                UI_DataGridView.DataSource = _bindingSource;
+                UI_DataGridView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                UI_DataGridView.Columns[3].HeaderText = "Total Mass";
+            }
+            else
+            {
+                _bindingSource.DataSource = null;
+                UI_DataGridView.DataSource = _bindingSource;
+            }
+
+            string pattern2 = @"[A-Z][a-z]|[A-Z]|[a-z]";
+            MatchCollection matchesForColor = Regex.Matches(userInput, pattern2);
+
+            int color_value = 0;
+            int discrepancy = 0;
+            foreach (Match item in matchesForColor)
+            {
+                foreach (var element in elements)
+                {
+                    if (element.Symbol == item.Value)
+                    {
+                        color_value++;
+                    }
+                    if (element.Symbol == $"{item.Value[0]}")
+                    {
+                        color_value++;
+                        discrepancy++; //increasing it so that the yellow color is displayed, the regex will take Hh as one item and make the backcolor red because H won't be equal to Hh,but if I just check for item[0] and increase the color value, it will make it green, so had to include discrepancy
+                    }
+                }
+
+            }
+
+            if (color_value == 0)
+                UI_MolarMass_Tbx.BackColor = Color.Red;
+            else if (color_value > 0 && color_value < matchesForColor.Count + discrepancy)
+                UI_MolarMass_Tbx.BackColor = Color.Yellow;
+            else
+                UI_MolarMass_Tbx.BackColor = Color.Green;
+
+            UI_MolarMass_Tbx.Text = $"{elements.Sum(x => x.MassInFormula)} g/mol";
+            //If empty then back to default
+            if (userInput.Length == 0)
+            {
+                UI_MolarMass_Tbx.BackColor = Color.White;
+                UI_MolarMass_Tbx.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// On Load event for the form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MMC_Load(object sender, EventArgs e)
+        {
+            GetData();//calling the API function to get the data
+            this.ActiveControl = UI_ChemicalFormula_tbx;
+            UI_DataGridView.RowHeadersVisible = false;
+        }
+
+        /// <summary>
+        /// This function will make a web call to get all the data as String
+        /// and then parse into the JSON object and making list out of it
+        /// </summary>
+        private async void GetData()
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();//new HTTP clietn object
+                string apiUrl = "https://apiweatherforcast.azurewebsites.net/api/AtomControllers";//url for API
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);//awiting till We got response back 
+                if (response.IsSuccessStatusCode)//if status code is 200 , then will populate the master Atom list
+                {
+                    string data = await response.Content.ReadAsStringAsync();//reading all the data
+                    _atomList = JsonConvert.DeserializeObject<List<Atom>>(data);//desearilizing the data using JSON Newston lib
+                }
+                else//if APPI call is unsuccessfull then will call a ReadFile function to read all the data from the .txt file 
+                {
+                    MessageBox.Show("API request failed: " + response.ReasonPhrase, "Error");
+                    ReadFile();
+                }
+            }
+            catch (HttpRequestException ex)//if APPI call is unsuccessfull then will call a ReadFile function to read all the data from the.txt file
+            {
+                MessageBox.Show("API request failed: " + ex.Message, "Error");
+                ReadFile();
+            }
+        }
+        /// <summary>
+        /// This is the backup method if , API fails to run it will call this method 
+        /// to fill the list 
+        /// </summary>
+        private void ReadFile()
+        {
+
+            MessageBox.Show("Now reading the file. ");//displaying the message to the user 
+            string data = Resources.AtomsData;//getting the content of file as string
+            _atomList = JsonConvert.DeserializeObject<List<Atom>>(data);//making a new list of  Atoms
+
+        }
+
+        /// <summary>
+        /// This function will sort all the data by Atom Name
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UI_SortByName_Btn_Click(object sender, EventArgs e)
+        {
+            //sorting all the elements in ascending order of the name and assigning it to the binding source
+            _bindingSource.DataSource = (from item in _atomList
+                                         orderby item.Name
+                                         select new
+                                         {
+                                             Atomic = item.AtomicNumber,
+                                             Name = item.Name,
+                                             Symbol = item.Symbol,
+                                             Mass = item.MolarMass
+                                         });
+            UI_DataGridView.DataSource = _bindingSource;
+
+            // changing column name and Making proper grid size
+            UI_DataGridView.Columns[0].HeaderText = "Atomic #";
+            UI_DataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        /// <summary>
+        /// This function will make a list of all the data where symbol contain only one letter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UI_SingleCharacterSymbols_Btn_Click(object sender, EventArgs e)
+        {
+            // selecting all the elements having Single char symbol and assigning it to BS 
+            _bindingSource.DataSource = (from item in _atomList 
+                                         where item.Symbol.Length == 1 
+                                         select new 
+                                         {Atomic = item.AtomicNumber, 
+                                          Name = item.Name,
+                                          Symbol = item.Symbol, 
+                                          Mass = item.MolarMass});
+            UI_DataGridView.DataSource = _bindingSource;
+
+            // changing column name and Making proper grid size
+            UI_DataGridView.Columns[0].HeaderText = "Atomic #";
+            UI_DataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+        }
+
+        /// <summary>
+        /// This function sort all the data on the basis of Atomic Number
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UI_SortByAtomic_btn_Click(object sender, EventArgs e)
+        {
+
+            // sorting by Atomic number and assigning it to the Data source
+            _bindingSource.DataSource = (from item in _atomList
+                                         orderby item.AtomicNumber
+                                         select new
+                                         {
+                                             Atomic = item.AtomicNumber,
+                                             Name = item.Name,
+                                             Symbol = item.Symbol,
+                                             Mass = item.MolarMass
+                                         });
+            UI_DataGridView.DataSource = _bindingSource;
+            
+            // changing column name and Making proper grid size
+            UI_DataGridView.Columns[0].HeaderText = "Atomic #";
+            UI_DataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+        }
+    }
+}
